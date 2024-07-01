02416caa8a36ff0922a6ebebdd71403c10e1e83d
Wellyngton Molinari <88794503+WellyngtonMolinari@users.noreply.github.com>
2024-07-01 15:53:20 -0300

    carrousel fix

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function resetProgressBar() {
+        // Reinicia a largura da barra de progresso para 0%
         progressBar.style.width = '0';
+        // Utiliza um setTimeout para garantir que o CSS termine a transição atual antes de iniciar uma nova
         setTimeout(() => {
-            progressBar.style.width = '100%';
-        }, 10);
+            progressBar.style.transition = 'none'; // Remove a transição para resetar imediatamente
+            progressBar.style.width = '100%'; // Define a largura para 100%
+            // Adiciona uma pequena espera e reinicia a transição
+            setTimeout(() => {
+                progressBar.style.transition = 'width 5s linear'; // Reaplica a transição
+            }, 50); // Pequeno delay para garantir a aplicação do reset
+        }, 10); // Pequeno delay para permitir que a largura vá para 0
     }
 
+    // Inicializa a barra de progresso e o carrossel
     setInterval(showNextImage, intervalTime);
     resetProgressBar();
 });
