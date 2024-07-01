b4622627c507d44ff2e8ae5f1ceaf7ed980f20e8
Wellyngton Molinari <88794503+WellyngtonMolinari@users.noreply.github.com>
2024-07-01 15:49:48 -0300

    carrousel added

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Exemplo de código JavaScript para adicionar interações
-    // Aqui você pode adicionar scripts para funcionalidades específicas
+    const images = document.querySelectorAll('.carousel-images img');
+    const progressBar = document.querySelector('.progress-bar');
+    let currentIndex = 0;
+    const intervalTime = 5000; // 5 segundos
+
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        const offset = -currentIndex * 100;
+        document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+        resetProgressBar();
+    }
+
+    function resetProgressBar() {
+        progressBar.style.width = '0';
+        setTimeout(() => {
+            progressBar.style.width = '100%';
+        }, 10);
+    }
+
+    setInterval(showNextImage, intervalTime);
+    resetProgressBar();
 });
